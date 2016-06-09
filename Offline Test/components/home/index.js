53ca0f1b35eb085a28a91ae00b29a9b424b7a42b
Renato Teixeira <rteixeir@progress.com>
2016-06-09 16:58:27 +0000

    Changes from Views service

--- Offline Test/components/home/index.js
+++ Offline Test/components/home/index.js
@@ -9,3 +9,149 @@ app.home = kendo.observable({
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
 // END_CUSTOM_CODE_home
+(function(parent) {
+    var dataProvider = app.data.offlineTest,
+        fetchFilteredData = function(paramFilter, searchFilter) {
+            var model = parent.get('homeModel'),
+                dataSource = model.get('dataSource');
+
+            if (paramFilter) {
+                model.set('paramFilter', paramFilter);
+            } else {
+                model.set('paramFilter', undefined);
+            }
+
+            if (paramFilter && searchFilter) {
+                dataSource.filter({
+                    logic: 'and',
+                    filters: [paramFilter, searchFilter]
+                });
+            } else if (paramFilter || searchFilter) {
+                dataSource.filter(paramFilter || searchFilter);
+            } else {
+                dataSource.filter({});
+            }
+        },
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'dbo_Employees',
+                dataProvider: dataProvider
+            },
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            error: function(e) {
+                if (e.xhr) {
+                    alert(JSON.stringify(e.xhr));
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'FirstName': {
+                            field: 'FirstName',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+            serverFiltering: true,
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        homeModel = kendo.observable({
+            dataSource: dataSource,
+            itemClick: function(e) {
+
+                app.mobileApp.navigate('#components/home/details.html?uid=' + e.dataItem.uid);
+
+            },
+            addClick: function() {
+                app.mobileApp.navigate('#components/home/add.html');
+            },
+            detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    dataSource = homeModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+
+                if (!itemModel.FirstName) {
+                    itemModel.FirstName = String.fromCharCode(160);
+                }
+
+                homeModel.set('currentItem', null);
+                homeModel.set('currentItem', itemModel);
+            },
+            currentItem: null
+        });
+
+    parent.set('addItemViewModel', kendo.observable({
+        onShow: function(e) {
+            // Reset the form data.
+            this.set('addFormData', {
+                surname: '',
+                country: '',
+                city: '',
+                name: '',
+            });
+        },
+        onSaveClick: function(e) {
+            var addFormData = this.get('addFormData'),
+                dataSource = homeModel.get('dataSource');
+
+            dataSource.add({
+                LastName: addFormData.surname,
+                Country: addFormData.country,
+                City: addFormData.city,
+                FirstName: addFormData.name,
+            });
+
+            dataSource.one('change', function(e) {
+                app.mobileApp.navigate('#:back');
+            });
+
+            dataSource.sync();
+        }
+    }));
+
+    if (typeof dataProvider.sbProviderReady === 'function') {
+        dataProvider.sbProviderReady(function dl_sbProviderReady() {
+            parent.set('homeModel', homeModel);
+        });
+    } else {
+        parent.set('homeModel', homeModel);
+    }
+
+    parent.set('onShow', function(e) {
+        var param = e.view.params.filter ? JSON.parse(e.view.params.filter) : null;
+
+        fetchFilteredData(param);
+    });
+})(app.home);
+
+// START_CUSTOM_CODE_homeModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_homeModel
